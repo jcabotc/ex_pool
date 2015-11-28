98af03e03c3dca819747f35211885649a26de433
Jaime Cabot Campins <jcabot@gmail.com>
2015-11-28 17:55:15 +0700

    simpler state_tests to not repeat Workers and Queue tests

--- test/ex_pool/pool/state_test.exs
+++ test/ex_pool/pool/state_test.exs
@@ -8,29 +8,23 @@ defmodule ExPool.Pool.StateTest do
   end
 
   setup do
-    state = State.new(worker_mod: TestWorker, size: 2)
+    state = State.new(worker_mod: TestWorker, size: 1)
 
     {:ok, %{state: state}}
   end
 
   test "#get_worker, #put_worker", %{state: state} do
     assert {:ok, {worker_1, state}}  = State.get_worker(state)
-    assert {:ok, {_worker_2, state}} = State.get_worker(state)
     assert {:empty, ^state}          = State.get_worker(state)
 
     state = State.put_worker(state, worker_1)
-
     assert {:ok, {^worker_1, _state}} = State.get_worker(state)
   end
 
   test "#enqueue, #pop_from_queue", %{state: state} do
-    assert {:empty, state} = State.pop_from_queue(state)
+    state = State.enqueue(state, :from)
 
-    state = State.enqueue(state, :from_1)
-    state = State.enqueue(state, :from_2)
-
-    assert {:ok, {:from_1, state}} = State.pop_from_queue(state)
-    assert {:ok, {:from_2, state}} = State.pop_from_queue(state)
+    assert {:ok, {:from, state}} = State.pop_from_queue(state)
     assert {:empty, _state}        = State.pop_from_queue(state)
   end
 end
