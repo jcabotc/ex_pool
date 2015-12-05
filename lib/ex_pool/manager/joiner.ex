8665a060b27980255ea650aee39910c0f579681d
Jaime Cabot Campins <jcabot@gmail.com>
2015-12-05 18:09:59 +0700

    Joiner

--- lib/ex_pool/manager/joiner.ex
+++ lib/ex_pool/manager/joiner.ex
@@ -1,2 +1,38 @@
 defmodule ExPool.Manager.Joiner do
+  alias ExPool.State
+
+  def join(state, worker) do
+    case Process.alive?(worker) do
+      true  -> check_in(state, worker)
+      false -> {:dead_worker, state}
+    end
+  end
+
+  defp check_in(state, worker) do
+    case State.pop_from_queue(state) do
+      {:ok, {from, state}} -> check_out(state, worker, from)
+      {:empty, state}      -> stash(state, worker)
+    end
+  end
+
+  defp check_out(state, worker, {pid, _ref} = from) do
+    {:ok, ref} = State.ref_from_item(state, {:waiting, pid})
+
+    state = state
+            |> State.remove_monitor({:waiting, pid})
+            |> State.add_monitor({:in_use, worker}, ref)
+
+    {:check_out, {from, worker, state}}
+  end
+
+  defp stash(state, worker) do
+    {:ok, ref} = State.ref_from_item(state, {:in_use, worker})
+    Process.demonitor(ref)
+
+    state = state
+            |> State.remove_monitor({:in_use, worker})
+            |> State.return_worker(worker)
+
+    {:ok, state}
+  end
 end
---
+++ test/ex_pool/manager/joiner_test.exs
@@ -0,0 +1,55 @@
+defmodule ExPool.Manager.JoinerTest do
+  use ExUnit.Case
+
+  alias ExPool.State
+  alias ExPool.Manager.Joiner
+
+  defmodule TestWorker do
+    def start_link(_opts \\ []), do: Agent.start_link(fn -> :ok end)
+  end
+
+  setup do
+    state = State.new(worker_mod: TestWorker, size: 2)
+    {:ok, worker} = TestWorker.start_link
+
+    {:ok, %{worker: worker, state: state}}
+  end
+
+  def new_pid do
+    {:ok, pid} = Agent.start_link(fn -> :ok end)
+    pid
+  end
+
+  test "join/2 with a dead worker", %{worker: worker, state: state} do
+    Agent.stop(worker)
+
+    assert {:dead_worker, ^state} = Joiner.join(state, worker)
+  end
+
+  test "join/2 with a process waiting", %{worker: worker, state: state} do
+    pid  = new_pid
+    from = {pid, :from_ref}
+    ref  = Process.monitor(pid)
+
+    state = State.enqueue(state, from)
+    state = State.add_monitor(state, {:waiting, pid}, ref)
+
+    assert {:check_out, {^from, ^worker, state}} = Joiner.join(state, worker)
+
+    assert :not_found = State.ref_from_item(state, {:waiting, pid})
+    assert {:ok, ^ref} = State.ref_from_item(state, {:in_use, worker})
+    assert State.available_workers(state) == 0
+  end
+
+  test "join/2 with no process waiting", %{worker: worker, state: state} do
+    pid = new_pid
+    ref = Process.monitor(pid)
+
+    state = State.add_monitor(state, {:in_use, worker}, ref)
+
+    assert {:ok, state} = Joiner.join(state, worker)
+
+    assert :not_found = State.ref_from_item(state, {:in_use, worker})
+    assert State.available_workers(state) == 1
+  end
+end
